474236a661e5e5f4ff0fee85cc324b09dd35b1b3
Poul Møller Hansen <ph@pbnet.dk>
2024-12-02 14:04:35 +0100

    Added orderbridge docker files

---
+++ knahrvorn/roles/docker/files/home/pmh/docker/orderbridge/update-deployment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function _check_env {
+  if [[ ! -v "$1" ]]; then
+    echo "$1 is not set"
+    exit 1
+  fi
+}
+
+_check_env MAILBOX
+_check_env MS_APP_ID
+_check_env MS_SECRET_TEXT
+_check_env MS_TENANT_ID
+_check_env PROD
+
+docker compose pull
+docker compose down && docker compose up -d
