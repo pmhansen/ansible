a1f690de00957a8f92930bce01607cf3cfb07fab
Poul Møller Hansen <ph@pbnet.dk>
2025-06-13 13:41:53 +0200

    Only reload consul at change

--- knahrvorn/roles/nomad/templates/etc/consul.d/scripts/update-pg-role.sh
+++ knahrvorn/roles/nomad/templates/etc/consul.d/scripts/update-pg-role.sh
@@ -9,24 +9,46 @@ export PGDATABASE=postgres
 export PGUSER={{ postgres.user }}
 export PGPASSWORD={{ postgres.pass }}
 
+NEED_RELOAD=0
+
 for i in "${!CLUSTERS[@]}"; do
 
   ROLE=$(psql -p ${PORTS[i]} -tAc "SELECT pg_is_in_recovery();" 2>/dev/null)
+  MASTER_FILE="$CONSUL_DIR/services/pg-master-${CLUSTERS[i]}.hcl"
+  SLAVE_FILE="$CONSUL_DIR/services/pg-slave-${CLUSTERS[i]}.hcl"
 
   if [[ "$ROLE" == "f" ]]; then
-    # Register as master
-    cp /etc/consul.d/templates/pg-master-${CLUSTERS[i]}.tmpl $CONSUL_DIR/services/pg-master-${CLUSTERS[i]}.hcl
-    # Unregister as slave
-    rm -f "$CONSUL_DIR/services/pg-slave-${CLUSTERS[i]}.hcl"
+
+    # Register as master and remove as slave if not already
+    if [[ ! -f "$MASTER_FILE" ]]; then
+      cp /etc/consul.d/templates/pg-master-${CLUSTERS[i]}.tmpl "$MASTER_FILE"
+      NEED_RELOAD=1
+    fi
+    
+    if [[ -f "$SLAVE_FILE" ]]; then
+      rm -f "$SLAVE_FILE"
+      NEED_RELOAD=1
+    fi
+
   elif [[ "$ROLE" == "t" ]]; then
-    # Register as slave
-    cp /etc/consul.d/templates/pg-slave-${CLUSTERS[i]}.tmpl $CONSUL_DIR/services/pg-slave-${CLUSTERS[i]}.hcl
-    # Unregister as master
-    rm -f "$CONSUL_DIR/services/pg-master-${CLUSTERS[i]}.hcl"
+    # Register as slave and remove as master if not already
+
+    if [[ ! -f "$SLAVE_FILE" ]]; then
+      cp /etc/consul.d/templates/pg-slave-${CLUSTERS[i]}.tmpl "$SLAVE_FILE"
+      NEED_RELOAD=1
+    fi
+    
+    if [[ -f "$MASTER_FILE" ]]; then
+      rm -f "$MASTER_FILE"
+      NEED_RELOAD=1
+    fi
+
   else
     echo "Unable to determine role"
     exit 1
   fi
 done
 
-consul reload
+if [[ "$NEED_RELOAD" -eq 1 ]]; then
+  consul reload
+fi
